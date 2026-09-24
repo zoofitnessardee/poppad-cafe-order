fbbd1e008d364f5420707abcd11e6dd6b9cf141a
zoofitnessardee <zoofitnessardee@gmail.com>
2026-09-24 21:52:01 +0100

    Update orderApiUrl in POPPAD_ORDER_CONFIG

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.POPPAD_ORDER_CONFIG = {
   businessName: "POPPAD CAFÉ",
-  orderApiUrl: "",
+  orderApiUrl: "https://script.google.com/macros/s/AKfycbwfZFvdSkFF7rCw89zY15EYTc0-_AOHLJygVus_NpjhfjCks18H0yXLutG4P21TlQg/exec",
   inactivityResetMs: 90000
 };
